b59d2f59fbbf1aa0772e6660ca64c80c88d52df9
David <nakdt9@gmail.com>
2025-05-04 00:02:47 +0200

    accidentally added tensorflow through git

--- main.py
+++ main.py
@@ -15,3 +15,6 @@ from tensorflow.keras.layers import Dense, Embedding, LSTM, Dropout, MaxPooling1
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
+
+vocab_size = 10000
+(X_train, y_train), (X_test, y_test) = imdb.load_data(num_words = vocab_size)
